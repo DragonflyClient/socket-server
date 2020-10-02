389e747b7b04ff37ce69e349abeb8b93dab6f466
incxption <theincxption@gmail.com>
2020-10-02 14:53:16 +0200

    :sparkles: Group online time by months

--- core/src/main/kotlin/net/dragonfly/socket/server/statistics/Statistics.kt
+++ core/src/main/kotlin/net/dragonfly/socket/server/statistics/Statistics.kt
@@ -4,6 +4,7 @@ import com.mongodb.client.model.*
 import kotlinx.coroutines.runBlocking
 import net.dragonfly.socket.server.Database
 import net.dragonfly.socket.server.session.Session
+import java.text.SimpleDateFormat
 
 object Statistics {
 
@@ -12,9 +13,15 @@ object Statistics {
         val millis = System.currentTimeMillis() - firstActiveTime
         val time = millis / (1000 * 60) // in minutes
 
+        val sdf = SimpleDateFormat("MM/yyyy")
+        val month = sdf.format(System.currentTimeMillis())
+
         Database.statisticsCollection.updateOne(
             Filters.eq("dragonflyUUID", session.account.uuid),
-            Updates.inc("onlineTime", time),
+            Updates.combine(
+                Updates.inc("onlineTime.total", time),
+                Updates.inc("onlineTime.$month", time)
+            ),
             UpdateOptions().upsert(true)
         )
     }
